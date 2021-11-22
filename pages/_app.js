44b00de595ac7eeeb76d6cf1cedabdf00e1e0589
Henrique <henrique@usebom.com>
2021-11-22 10:31:13 -0300

    add: loading screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,26 @@
+import React from "react"
 import "../styles/theme.css";
 import "../styles/fluiditype.css";
 import "../styles/styles.css";
 import "../styles/equalizer.css";
+import LoadingScreen from "../components/loadingScreen";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setLoading(true);
+    setTimeout(() => setLoading(false), 800);
+  }, []);
+  return (
+    <>
+      {!loading ? (
+        <React.Fragment>
+          <Component {...pageProps} />
+        </React.Fragment>
+      ) : (
+        <LoadingScreen />
+      )}
+    </>
+  );
 }
