a4b4062b8eccd4e54374ce3398b02a4e81fba2f4
Henrique <henriquepett@gmail.com>
2022-05-05 21:24:59 -0300

    upd: job and company name

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,43 @@
-import React from "react";
-import Motion from "../components/motion";
-import Metadata from "../components/metadata";
-import Link from "next/link";
-import Main from "../components/innerWrapper";
+import React from 'react'
+import Motion from '../components/motion'
+import Metadata from '../components/metadata'
+import Link from 'next/link'
+import Main from '../components/innerWrapper'
 
-import MainWrapper from "../components/mainWrapper";
+import MainWrapper from '../components/mainWrapper'
 
 const Home = () => (
   <Motion>
     <MainWrapper>
       <Metadata
-        title="Henrique P."
-        description="Software engineer working as a Full Stack developer"
-        name="Henrique Pett"
+        title='Henrique P.'
+        description='Data Scientist'
+        name='Henrique Pett'
       />
       <Main>
-        <h1 className="heading2Xl">Hey, I'm Henrique! </h1>
+        <h1 className='heading2Xl'>Hey, I'm Henrique! </h1>
         <p>
-          and I'm a Software engineer working as a Full Stack developer at {''}
-          <Link href="https://bom.capital/" as="https://bom.capital/">
-            <a className="internalPassage">Bom</a>
+          and I'm a Data scientist working at {''}
+          <Link href='https://bom.capital/' as='https://bom.capital/'>
+            <a className='internalPassage'>Bom Capital</a>
           </Link>
         </p>
-        <div className="contentWrapper">
+        <div className='contentWrapper'>
           <section>
             <ul>
-              <li className="blog">
-                <Link href="/about" as="/about">
-                  <a className="internalPassage">About</a>
+              <li className='blog'>
+                <Link href='/about' as='/about'>
+                  <a className='internalPassage'>About</a>
                 </Link>
               </li>
-              <li className="blog">
-                <Link href="/projects" as="/projects">
-                  <a className="internalPassage">Projects</a>
+              <li className='blog'>
+                <Link href='/projects' as='/projects'>
+                  <a className='internalPassage'>Projects</a>
                 </Link>
               </li>
-              <li className="blog">
-                <Link href="/writings" as="/writings">
-                  <a className="internalPassage">Writings</a>
+              <li className='blog'>
+                <Link href='/writings' as='/writings'>
+                  <a className='internalPassage'>Writings</a>
                 </Link>
               </li>
             </ul>
@@ -46,6 +46,6 @@ const Home = () => (
       </Main>
     </MainWrapper>
   </Motion>
-);
+)
 
-export default Home;
+export default Home
